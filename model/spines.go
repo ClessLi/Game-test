4596ba6f41cfe24c4fee625c3be44de74b6ee794
ClessLi <316600949@qq.com>
2019-12-02 18:11:12 +0800

    创建尖刺类和墙类

---
+++ model/spines.go
@@ -0,0 +1,11 @@
+package model
+
+const (
+	SpinesHeight float32 = 15
+	SpinesWidth  float32 = 15
+)
+
+//尖刺类
+type Spines struct {
+	GameObj
+}
---
+++ model/wall.go
@@ -0,0 +1,11 @@
+package model
+
+const (
+	WallHeight float32 = 18
+	WallWidth  float32 = 61
+)
+
+//墙类
+type Wall struct {
+	GameObj
+}
